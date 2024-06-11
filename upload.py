ff669b8b86c510d8f4aeac555f11544f7489f5ab
SmoothTurtle872 <whatleyfamily1@gmail.com>
2024-06-11 10:36:04 +0930

    Updated the program to auto zip the pack

--- upload.py
+++ upload.py
@@ -1,5 +1,22 @@
+import zipfile
+import os
+
 def main():
-  print("It works!")
+  walkedFiles = [file for file in os.walk("assets")]
+  files = []
+  for file in walkedFiles:
+    concatinatedFile = file[0]
+    for path in file[2]:
+      concatinatedFile = concatinatedFile + "\\" + path
+      files.append(concatinatedFile)
+      concatinatedFile = file[0]
+  
+  zipFile = zipfile.ZipFile("pack.zip","w",zipfile.ZIP_DEFLATED)
+  zipFile.write("pack.mcmeta")
+  zipFile.write("pack.png")
+  for file in files:
+    zipFile.write(file)
+  zipFile.close()
 
 if __name__ == "__main__":
   main()
